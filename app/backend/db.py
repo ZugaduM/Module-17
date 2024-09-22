13335209bf71e1f64189f24a9720589534b8fc94
Geldion <zugadum@mail.ru>
2024-09-22 23:52:34 +0300

    Crete db.py and write start code in it

---
+++ app/backend/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import Column, Integer, String
+
+engine = create_engine("sqlite:///taskmanager.db", echo=True)
+
+local_session = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
